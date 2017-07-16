16279035a4cc072806315896c550fcf70d995a81
Luis Jacome <luis.jacome@atrums.com>
2017-07-16 09:58:55 -0500

    quitando npm ngh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
 import { Component } from '@angular/core';
 
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { AngularFireAuth } from 'angularfire2/auth';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'IQ-MEDI';
+  mensajeError: string;
+  imageIqMedi: string;
+  formLogin: FormGroup;
+  formRegistrar: FormGroup;
+
+  constructor(
+    private fblogin: FormBuilder,
+    private fbregistrar: FormBuilder,
+    private abfauth: AngularFireAuth
+  ) {
+    this.crearComponenteLogin();
+    this.crearComponenteRegistrar();
+    this.imageIqMedi = '/assets/resources/iq-medi.png';
+  }
+
+  crearComponenteLogin() {
+    this.formLogin = this.fblogin.group({
+      email: ['', Validators.required],
+      contrasenia: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(20)
+      ])]
+    })
+  }
+
+  crearComponenteRegistrar() {
+    this.formRegistrar = this.fbregistrar.group({
+      email: ['', Validators.required],
+      contrasenia: ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(20)
+      ])]
+    })
+  }
+
+  login() {
+    console.log('Login');
+    // console.log(this.formLogin.value);
+    this.limpiarLogin();
+  }
+
+  registrar() {
+    console.log('Registrar');
+    // console.log(this.formRegistrar.value);
+    this.abfauth.auth.createUserWithEmailAndPassword(this.formRegistrar.value.email, this.formRegistrar.value.contrasenia)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch(error => {
+        console.log(error.message);
+        console.log(error)
+        alert(error.message);
+      });
+    this.limpiarRegistrar();
+  }
+
+  limpiarLogin() {
+    console.log('Limpiando Formulario');
+    this.formLogin.reset();
+  }
+
+  limpiarRegistrar() {
+    console.log('Limpiando Formulario');
+    this.formRegistrar.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { WorkComponent } from './work/work.component';
+
+import { AppRoutingModule } from './app.router.module';
+import { HomeModule } from './home/home.module';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuth } from 'angularfire2/auth'
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDz6Z7GWHd3qtjH19l_q3DR63daZrIihIY',
+  authDomain: 'iq-medi.firebaseapp.com',
+  databaseURL: 'https://iq-medi.firebaseio.com',
+  storageBucket: 'iq-medi.appspot.com',
+  messagingSenderId: '303493590025'
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    WorkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFireAuth],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
